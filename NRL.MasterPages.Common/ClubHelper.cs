2b8db9fbd2cae9ec32fdf699612e0daeb3e4b073
RicardoCasqueteTicketek <ricardoc@ticketek.com.au>
2014-08-29 17:05:38 +1000

    Added the Title and the Dinamic Injection of JQuery

--- NRL.MasterPages.Common/ClubHelper.cs
+++ NRL.MasterPages.Common/ClubHelper.cs
@@ -16,5 +16,27 @@
 
             return string.Empty;
         }
+
+        public static bool IsMarkOnlyUpRequest<TModel>(this HtmlHelper<TModel> htmlHelper)
+        {
+            return HttpContext.Current.Request.Url.LocalPath.ToLower().EndsWith("/home/blank");
+        }
+
+        public static String Title<TModel>(this HtmlHelper<TModel> htmlHelper)
+        {
+            var requestedClub = ClubConfig.Instance.FirstOrDefault(club => club.Host == HttpContext.Current.Request.Url.Host);
+            if (requestedClub != null)
+                return string.Format("{0} Memberships Portal", requestedClub.Name.capitlizeFirst());
+
+            return string.Empty;
+        }
+
+        private static string capitlizeFirst(this string inputString)
+        {
+            if (string.IsNullOrEmpty(inputString) || inputString.Length <= 1)
+                return string.Empty;
+
+            return string.Format("{0}{1}", char.ToUpper(inputString[0]), inputString.Substring(1));
+        }
     }
 }
